9c2ec09a27ce5eb9f7d489ac5e662df9d22b78ea
Anastasia Boldovskaya <kapibaara@mail.ru>
2019-02-03 14:43:51 +0300

    added redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { rootReduser } from './store/reducers/rootReducer';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const store = createStore(rootReduser, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();

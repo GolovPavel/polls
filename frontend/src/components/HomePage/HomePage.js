6ea6df6e01f37836c22031adab1a5079c0e5067c
Anastasia Boldovskaya <kapibaara@mail.ru>
2019-02-10 18:37:25 +0300

    fixed HomePage component

---
+++ frontend/src/components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <h1>Super-duper amazing app! Enjoy!</h1>
+    </div>
+  );
+};
+
+export default HomePage;

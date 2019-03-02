5b289dbeb253946fe98b5a59a4753f24582157d7
Anastasia Boldovskaya <kapibaara@mail.ru>
2019-03-02 22:57:20 +0300

    added validators to SignUp form

--- frontend/src/components/Auth/Signup/SignupForm.js
+++ frontend/src/components/Auth/Signup/SignupForm.js
@@ -1,61 +1,103 @@
 import React, {Component} from 'react';
 import { Form, Icon, Input, Button } from 'antd';
 import { Link } from "react-router-dom";
+import { validateFunction } from "../../../helpers/validators";
+
 import "../Auth.css";
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullName: {
+        value: ""
+      },
+      username: {
+        value: ""
+      },
+      email: {
+        value: ""
+      },
+      password: {
+        value: ""
+      }
+    }
+  }
+
+  handleInputChange = event => {
+    const inputValue = event.target.value;
+    const inputName = event.target.name;
+
+    this.setState({
+      [inputName]: {
+        value: inputValue,
+        ...validateFunction(inputName, inputValue)
+      }
+    })
+  };
+
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const { fullName, username, email, password } = this.state;
+    const isFormValid = fullName.validateStatus === "success" && username.validateStatus === "success" && email.validateStatus === "success" && password.validateStatus === "success";
 
     return (
       <Form className="signup-form">
-        <Form.Item>
-          {getFieldDecorator('fullName', {
-            rules: [{ required: true, message: 'Please input your fullname!' }],
-          })(
-            <Input
-              prefix={<Icon type="smile" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              name="fullName"
-              placeholder="Full Name"
-            />
-          )}
+        <Form.Item
+          validateStatus={fullName.validateStatus}
+          help={fullName.errorMsg}
+        >
+          <Input
+            prefix={<Icon type="smile" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            name="fullName"
+            placeholder="Full Name"
+            autoComplete="off"
+            onChange={this.handleInputChange}
+          />
         </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              name="username"
-              placeholder="Username"
-            />
-          )}
+        <Form.Item
+          validateStatus={username.validateStatus}
+          help={username.errorMsg}
+        >
+          <Input
+            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            name="username"
+            autoComplete="off"
+            placeholder="Username"
+            onChange={this.handleInputChange}
+          />
         </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('email', {
-            rules: [{ required: true, message: 'Please input your email!' }],
-          })(
-            <Input
-              prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              name="email"
-              placeholder="Email"
-            />
-          )}
+        <Form.Item
+          validateStatus={email.validateStatus}
+          help={email.errorMsg}
+        >
+          <Input
+            prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            name="email"
+            placeholder="Email"
+            autoComplete="off"
+            onChange={this.handleInputChange}
+          />
         </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
+        <Form.Item
+          validateStatus={password.validateStatus}
+          help={password.errorMsg}
+        >
             <Input
               prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
               type="password"
               name="password"
               placeholder="Password"
+              onChange={this.handleInputChange}
             />
-          )}
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit" className="auth-form-button">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="auth-form-button"
+            disabled={!isFormValid}
+          >
             Sign up
           </Button>
           Or <Link to="/login">login now!</Link>
---
+++ frontend/src/helpers/validators.js
@@ -0,0 +1,120 @@
+import {
+  NAME_MIN_LENGTH,
+  NAME_MAX_LENGTH,
+  USERNAME_MIN_LENGTH,
+  USERNAME_MAX_LENGTH,
+  EMAIL_MAX_LENGTH,
+  EMAIL_MASK,
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_MAX_LENGTH
+} from "./constants";
+
+export const validateFunction = (fieldName, fieldValue) => {
+  switch (fieldName) {
+    case "fullName":
+      return validateFullName(fieldValue);
+    case "username":
+      return validateUserName(fieldValue);
+    case "email":
+      return validateEmail(fieldValue);
+    case "password":
+      return validatePassword(fieldValue);
+    default:
+      return {
+        validateStatus: "success",
+        errorMsg: null
+      }
+  }
+};
+
+const validateFullName = fullName => {
+  if (fullName.length < NAME_MIN_LENGTH) {
+    return {
+      validateStatus: "error",
+      errorMsg: `Name is too short.(Minimum ${NAME_MIN_LENGTH} characters needed.)`
+    }
+  }
+
+  if (fullName.length > NAME_MAX_LENGTH) {
+    return {
+      validateStatus: "error",
+      errorMsg: `Name is too long.(Maximum ${NAME_MAX_LENGTH} characters allowed.)`
+    }
+  }
+
+  return {
+    validateStatus: "success",
+    errorMsg: null
+  }
+};
+
+const validateUserName = username => {
+  if (username.length < USERNAME_MIN_LENGTH) {
+    return {
+      validateStatus: "error",
+      errorMsg: `Username is too short.(Minimum ${USERNAME_MIN_LENGTH} characters needed.)`
+    }
+  }
+
+  if (username.length > USERNAME_MAX_LENGTH) {
+    return {
+      validateStatus: "error",
+      errorMsg: `Username is too long.(Maximum ${USERNAME_MAX_LENGTH} characters allowed.)`
+    }
+  }
+
+  return {
+    validateStatus: "success",
+    errorMsg: null
+  }
+};
+
+const validateEmail = email => {
+  if (!email) {
+    return {
+      validateStatus: "error",
+      errorMsg: "Email may not be empty"
+    }
+  }
+
+  if (!EMAIL_MASK.test(email)) {
+    return {
+      validateStatus: "error",
+      errorMsg: "Email is invalid"
+    }
+  }
+
+  if (email.length > EMAIL_MAX_LENGTH) {
+    return {
+      validateStatus: "error",
+      errorMsg: `Email is too long.(Maximum ${EMAIL_MAX_LENGTH} characters allowed.)`
+    }
+  }
+
+  return {
+    validateStatus: "success",
+    errorMsg: null
+  }
+};
+
+const validatePassword = password => {
+  if (password.length < PASSWORD_MIN_LENGTH) {
+    return {
+      validateStatus: "error",
+      errorMsg: `Password is too short.(Minimum ${PASSWORD_MIN_LENGTH} characters needed.)`
+    }
+  }
+
+  if (password.length > PASSWORD_MAX_LENGTH) {
+    return {
+      validateStatus: "error",
+      errorMsg: `Password is too long.(Maximum ${PASSWORD_MAX_LENGTH} characters allowed.)`
+    }
+  }
+
+  return {
+    validateStatus: "success",
+    errorMsg: null
+  }
+};
+
